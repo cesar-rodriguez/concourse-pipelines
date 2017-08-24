0284f3ba6de42a83fae6b5c41136e46e634f8f0b
Cesar Rodriguez <therasec@gmail.com>
2017-08-24 16:57:23 -0400

    adds test for terraform fmt

--- ci/scripts/scan.sh
+++ ci/scripts/scan.sh
@@ -7,8 +7,16 @@ set -eux
 
 cd infrastructure-repo
 
+# Check if code is properly formatted
+format_status=$(terraform fmt | wc -l)
+if [ $format_status -gt 0 ]
+then
+    echo "Scan failed as templates are not formatted. Please run 'terraform fmt'"
+    exit 1
+fi
+
 # "Scanning" terraform
 for env in $(ls environments | grep '.tfvars')
 do
-  tflint --error-with-issues --fast --var-file=environments/$env
+    tflint --error-with-issues --fast --var-file=environments/$env
 done
