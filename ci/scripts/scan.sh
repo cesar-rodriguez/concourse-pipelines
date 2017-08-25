e8e7e8b801b7b6652ca05c6431a0663dbd1deedb
Cesar Rodriguez <therasec@gmail.com>
2017-08-24 22:18:49 -0400

    adds echo of env

--- ci/scripts/scan.sh
+++ ci/scripts/scan.sh
@@ -10,5 +10,6 @@ cd infrastructure-repo
 # "Scanning" terraform
 for env in $(ls environments | grep '.tfvars')
 do
+    echo "****************** SCANNING $env ******************"
     tflint --error-with-issues --fast --var-file=environments/$env
 done
