ee0a0d332d3de0c6d1cbd489247051d68af48631
Cesar Rodriguez <therasec@gmail.com>
2017-08-24 22:29:47 -0400

    adds color to text

--- ci/scripts/format.sh
+++ ci/scripts/format.sh
@@ -5,14 +5,18 @@
 
 set -e
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
 cd infrastructure-repo
 
 # Check if code is properly formatted
 format_status=$(terraform fmt | wc -l)
 if [ $format_status -gt 0 ]
 then
-    echo "Scan failed as templates are not formatted. Please run 'terraform fmt'"
+    echo -e "${RED}[SCAN] failed: Templates are not formatted. Please run 'terraform fmt'"
     exit 1
 else
+    echo -e "${GREEN}[SCAN] successful"
     exit 0
 fi
