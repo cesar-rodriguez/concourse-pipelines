5d7be81cd883f4e47c58032430995bc40da577c7
Cesar Rodriguez <therasec@gmail.com>
2017-08-26 23:42:25 -0400

    adds support for multiple aws accounts on vault

--- vault-setup.sh
+++ vault-setup.sh
@@ -16,29 +16,36 @@ echo "Authenticating to Vault"
 export VAULT_ADDR=${VAULT_ADDR}
 vault auth ${VAULT_TOKEN}
 
-echo "Get AWS IAM access keys for Vault"
-set +e
-aws iam delete-access-key --user-name vault-iam --access-key-id $(aws iam list-access-keys --user-name vault-iam | jq -r '.AccessKeyMetadata[0].AccessKeyId')
-set -e
-KEYS=$(aws iam create-access-key --user-name vault-iam)
+environments='stage prod'
+for env in $environments
+do
+    AWS_PROFILE=$env
+    export AWS_PROFILE=${AWS_PROFILE}
+    echo "Get AWS IAM access keys for Vault"
+    set +e
+    aws iam delete-access-key --user-name vault-iam --access-key-id $(aws iam list-access-keys --user-name vault-iam | jq -r '.AccessKeyMetadata[0].AccessKeyId')
+    set -e
+    KEYS=$(aws iam create-access-key --user-name vault-iam)
 
-echo "Mount AWS backend and demo role"
-vault mount -path=aws/therasecaws aws
-vault write aws/therasecaws/config/root \
-    access_key=$(echo $KEYS | jq -r '.AccessKey.AccessKeyId') \
-    secret_key=$(echo $KEYS | jq -r '.AccessKey.SecretAccessKey') \
-    region=us-east-1
+    echo "Mount AWS backend and admin role"
+    vault mount -path=aws/therasec-${AWS_PROFILE} aws
+    vault write aws/therasec-${AWS_PROFILE}/config/root \
+        access_key=$(echo $KEYS | jq -r '.AccessKey.AccessKeyId') \
+        secret_key=$(echo $KEYS | jq -r '.AccessKey.SecretAccessKey') \
+        region=us-east-1
 
-echo "Create demo role"
-vault write aws/therasecaws/roles/aws-global-admin \
-    arn=arn:aws:iam::445730574438:role/adfs-admin
-echo 'path "aws/therasecaws/sts/aws-global-admin"{
-    capabilities = ["read"]
-}' | vault policy-write aws-sts-therasecaws-admin -
+    echo "Create admin role"
+    vault write aws/therasec-${AWS_PROFILE}/roles/aws-global-admin \
+        arn=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName=="adfs-admin") | .Arn')
+    echo 'path "aws/therasec-${AWS_PROFILE}/sts/aws-global-admin"{
+        capabilities = ["read"]
+    }' | vault policy-write aws-sts-therasec-${AWS_PROFILE}-admin -
+done
+
 
 echo "Enable Approle"
 vault auth-enable approle
-vault write auth/approle/role/awspipelinerole bind_cidr_list=192.168.0.0/16,127.0.0.1/32 policies=aws-sts-therasecaws-admin token_num_uses=0 token_ttl=24h token_max_ttl=720h secret_id_ttl=24h
+vault write auth/approle/role/awspipelinerole bind_cidr_list=192.168.0.0/16,127.0.0.1/32 policies=aws-sts-therasec-stage-admin,aws-sts-therasec-prod-admin token_num_uses=0 token_ttl=24h token_max_ttl=720h secret_id_ttl=24h
 
 echo "Retrieving approle"
 vault read auth/approle/role/awspipelinerole/role-id
