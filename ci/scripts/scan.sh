9dcd9269cba9678655913e7a4a9cf891dd9313e5
Cesar Rodriguez <therasec@gmail.com>
2017-09-03 22:04:39 -0400

    adds tflint scan to prs

--- ci/scripts/scan.sh
+++ ci/scripts/scan.sh
@@ -3,9 +3,20 @@
 # Runs tflint against code
 #
 
+# Checking if test is going to be performed against pull request or main repo
+ls pull-request &>/dev/null
+if [ $? -eq 0 ]
+then
+    echo "Evaluating pull-request"
+    export TEST_DIR=pull-request
+else
+    echo "Evaluating infrastructure-repo"
+    export TEST_DIR=infrastructure-repo
+fi
+
 set -e
 
-cd infrastructure-repo
+cd $TEST_DIR
 
 # "Scanning" terraform
 for env in $(ls environments | grep '.tfvars')
