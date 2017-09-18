a93e3662bf095308f18f6b8b7449e6da9ec5b5f8
Cesar Rodriguez <therasec@gmail.com>
2017-09-18 05:35:06 -0400

    adds progress bar

--- scripts/get-tools.sh
+++ scripts/get-tools.sh
@@ -6,14 +6,16 @@ CONCOURSE_VERSION='v3.4.1'
 VAULT_VERSION='0.8.2'
 
 get_vault () {
-  wget -q https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip >/dev/null
+  wget https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip \
+    --progress=bar:force 2>&1 | tail -f -n +6
   sudo unzip vault_${VAULT_VERSION}_linux_amd64.zip -d /usr/local/bin/
   rm -f vault_${VAULT_VERSION}_linux_amd64.zip
   sudo chmod 0755 /usr/local/bin/vault
 }
 
 get_concourse () {
-  wget -q https://github.com/concourse/concourse/releases/download/${CONCOURSE_VERSION}/concourse_linux_amd64 >/dev/null
+  wget https://github.com/concourse/concourse/releases/download/${CONCOURSE_VERSION}/concourse_linux_amd64 \
+    --progress=bar:force 2>&1 | tail -f -n +6
   chmod 0755 concourse_linux_amd64
   sudo mv concourse_linux_amd64 /usr/local/bin/concourse
 }
