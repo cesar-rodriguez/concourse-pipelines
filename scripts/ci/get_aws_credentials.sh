27aee0658aaed5eb54d2cd35ceb5f6abb65db557
Cesar Rodriguez <therasec@gmail.com>
2017-09-19 22:12:43 -0400

    testing using parameter for role name

--- scripts/ci/get_aws_credentials.sh
+++ scripts/ci/get_aws_credentials.sh
@@ -5,13 +5,15 @@
 
 set -ex
 
+echo "*** Using role $PIPELINE_AWS_ROLE"
+
 environments=$(ls vault/aws)
 for env in $environments
 do
     echo "
     [$env]
-    aws_access_key_id = $(cat vault/aws/$env/sts/aws-global-admin.json | jq -r '.access_key')
-    aws_secret_access_key = $(cat vault/aws/$env/sts/aws-global-admin.json | jq -r '.secret_key')
-    aws_session_token = $(cat vault/aws/$env/sts/aws-global-admin.json | jq -r '.security_token')
+    aws_access_key_id = $(cat vault/aws/$env/sts/${PIPELINE_AWS_ROLE}.json | jq -r '.access_key')
+    aws_secret_access_key = $(cat vault/aws/$env/sts/${PIPELINE_AWS_ROLE}.json | jq -r '.secret_key')
+    aws_session_token = $(cat vault/aws/$env/sts/${PIPELINE_AWS_ROLE}.json | jq -r '.security_token')
     " >> aws-creds/credentials
 done
