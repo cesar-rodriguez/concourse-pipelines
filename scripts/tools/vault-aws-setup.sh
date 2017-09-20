236a0624b2e19ca44abd98dfc90e48d838d81cce
Cesar Rodriguez <therasec@gmail.com>
2017-09-20 09:40:35 -0400

    moves vault AWS setup script

--- scripts/tools/vault-aws-setup.sh
+++ scripts/tools/vault-aws-setup.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 #
-# Sets up a dev version of vault with app role and aws backend
-#
-#
+# Sets up vault AWS backend
+# Expects:
+#   - The aws cli and vault cli to be installed
+#   - AWS credentials file with 2 profiles named 'stage' and 'prod'
 
 set -ex
 
-echo "Starting dev Vault instance"
-VAULT_ADDR_NO_PROTOCOL='192.168.100.1:8200'
-VAULT_ADDR='http://192.168.100.1:8200'
+# Variables
+VAULT_ADDR_NO_PROTOCOL='192.168.100.4:8200'
+VAULT_ADDR='http://192.168.100.4:8200'
 VAULT_TOKEN='1234'
-vault server -dev -dev-root-token-id=${VAULT_TOKEN} -dev-listen-address=${VAULT_ADDR_NO_PROTOCOL} &>/dev/null &
+GITHUB_ORG='therasec'
+
 
 echo "Authenticating to Vault"
 export VAULT_ADDR=${VAULT_ADDR}
@@ -28,16 +30,16 @@ do
     KEYS=$(aws iam create-access-key --user-name vault-iam)
 
     echo "Mount AWS backend and admin role"
-    vault mount -path=aws/therasec-${AWS_PROFILE} aws
-    vault write aws/therasec-${AWS_PROFILE}/config/root \
+    vault mount -path=aws/${AWS_PROFILE} aws
+    vault write aws/${AWS_PROFILE}/config/root \
         access_key=$(echo $KEYS | jq -r '.AccessKey.AccessKeyId') \
         secret_key=$(echo $KEYS | jq -r '.AccessKey.SecretAccessKey') \
         region=us-east-1
 
     echo "Create admin role"
-    vault write aws/therasec-${AWS_PROFILE}/roles/aws-global-admin \
+    vault write aws/${AWS_PROFILE}/roles/aws-admin \
         arn=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName=="adfs-admin") | .Arn')
-    echo "path \"aws/therasec-${AWS_PROFILE}/sts/aws-global-admin\" {
+    echo "path \"aws/${AWS_PROFILE}/sts/aws-admin\" {
         capabilities = [\"read\"]
     }" | vault policy-write aws-sts-therasec-${AWS_PROFILE}-admin -
 done
@@ -45,8 +47,8 @@ done
 
 echo "Enable Approle"
 vault auth-enable approle
-vault write auth/approle/role/awspipelinerole bind_cidr_list=192.168.0.0/16,127.0.0.1/32 policies=aws-sts-therasec-stage-admin,aws-sts-therasec-prod-admin token_num_uses=0 token_ttl=24h token_max_ttl=720h secret_id_ttl=24h
+vault write auth/approle/role/teamconcourse bind_cidr_list=192.168.0.0/16,127.0.0.1/32 policies=aws-sts-therasec-stage-admin,aws-sts-therasec-prod-admin token_num_uses=0 token_ttl=24h token_max_ttl=720h secret_id_ttl=24h
 
 echo "Retrieving approle"
-vault read auth/approle/role/awspipelinerole/role-id
-vault write -f auth/approle/role/awspipelinerole/secret-id
+vault read auth/approle/role/teamconcourse/role-id
+vault write -f auth/approle/role/teamconcourse/secret-id
