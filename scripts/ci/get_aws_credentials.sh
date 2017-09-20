7ef13f8ebf9f1390e07b159f07d84e4574664ef0
Cesar Rodriguez <therasec@gmail.com>
2017-09-20 14:51:50 -0400

    adds comments

--- scripts/ci/get_aws_credentials.sh
+++ scripts/ci/get_aws_credentials.sh
@@ -9,7 +9,7 @@ if [ ! -z ${DEBUG_MODE} ]
 then
   if [ ${DEBUG_MODE} = "true" ]
   then
-    echo "DEBUG Mode"
+    echo "DEBUG MODE: Warning all STS tokens will be displayed in the Concourse UI"
     set -x
   fi
 fi
@@ -24,3 +24,5 @@ do
     aws_session_token = $(cat vault/aws/$env/sts/${PIPELINE_AWS_ROLE}.json | jq -r '.security_token')
     " >> aws-creds/credentials
 done
+
+echo "Stored STS tokens in aws-creds/credentials"
