b38329bf8feb3a7db2bd649754a7828f83ae73d9
Cesar Rodriguez <therasec@gmail.com>
2017-09-22 16:02:44 -0400

    adds initial cut at pr pipeline

---
+++ scripts/ci/terrascan-pr.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#
+# Installs and runs terrascan against infrastructure repository
+# Saves output to pull-request directory
+#
+
+set -e
+
+if [ ! -z ${DEBUG_MODE} ]
+then
+  if [ ${DEBUG_MODE} = "true" ]
+  then
+    echo "DEBUG MODE"
+    set -x
+  fi
+fi
+
+echo "Installing terrascan..."
+git clone https://github.com/therasec/terrascan.git
+cd terrascan
+pip install -r requirements.txt
+
+echo "Configuring terrascan..."
+echo "TERRAFORM_LOCATION = \"../../pull-request\"" > terrascan/settings.py
+
+echo "Running terrascan..."
+python -m unittest 2>&1 | tee ../pr-comment/comment
